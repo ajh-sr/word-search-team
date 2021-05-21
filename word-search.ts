696b9c2daef6fd778614aea90dddb81f7f5d036c
Adam Henley <adam.henley@shuttlerock.com>
2021-05-21 16:03:32 +1200

    Add findWord signature
    Signed-off-by: Adam Henley <adam.henley@shuttlerock.com>

--- word-search.ts
+++ word-search.ts
@@ -2,4 +2,8 @@ export default class WordSearch {
   public find(words: string[]) {
     throw new Error('Implement this function')
   }
+
+  private findWord(word: string, rowIndex: number = 0, colIndex: number = 0): RelativeCoordinate {
+
+  }
 }
