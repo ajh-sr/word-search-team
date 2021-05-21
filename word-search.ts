db0f271f0872eec73b915389ee1af6cfcfd31b37
Corben <corben.ferris@shuttlerock.com>
2021-05-21 16:07:45 +1200

    add ctor, find and move helper

--- word-search.ts
+++ word-search.ts
@@ -9,9 +9,22 @@ enum Direction {
   DOWN_RIGHT,
 }
 
+const move = {
+  [Direction.UP]: ({ x, y }: Point): Point => ({ x, y: y - 1}),
+  [Direction.DOWN]: ({ x, y }: Point): Point => ({ x, y: y + 1}),
+  [Direction.LEFT]: ({ x, y }: Point): Point => ({ x: x - 1, y }),
+  [Direction.RIGHT]: ({ x, y }: Point): Point => ({ x: x + 1, y }),
+  [Direction.UP_LEFT]: ({ x, y }: Point): Point => ({ x: x - 1, y: y - 1}),
+  [Direction.UP_RIGHT]: ({ x, y }: Point): Point => ({ x: x + 1, y: y - 1}),
+  [Direction.DOWN_LEFT]: ({ x, y }: Point): Point => ({ x: x - 1, y: y + 1}),
+  [Direction.DOWN_RIGHT]: ({ x, y }: Point): Point => ({ x: x + 1, y: y + 1}),
+}
+
 export default class WordSearch {
-  public find(words: string[]) {
-    throw new Error('Implement this function')
+  constructor(private grid: string[]) {}
+
+  public find(words: string[]): Result {
+    return words.reduce((acc, word) => ({ ...acc, [word]: this.findWord(word) }), {});
   }
 
   private findWord(word: string, rowIndex: number = 0, colIndex: number = 0): RelativeCoordinate {
