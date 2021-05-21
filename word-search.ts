01bfb83ac097f8e8451dee85c676e8cf9b91b652
Corben <corben.ferris@shuttlerock.com>
2021-05-21 15:59:28 +1200

    add direction enum

--- word-search.ts
+++ word-search.ts
@@ -1,3 +1,14 @@
+enum Direction {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+  UP_LEFT,
+  UP_RIGHT,
+  DOWN_LEFT,
+  DOWN_RIGHT,
+}
+
 export default class WordSearch {
   public find(words: string[]) {
     throw new Error('Implement this function')
