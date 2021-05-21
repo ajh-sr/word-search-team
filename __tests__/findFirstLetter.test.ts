7c1087580d1d46c3f2ad80bd37ed6b4d94a230d1
Naufan <naufan.rizal@shuttlerock.com>
2021-05-21 16:46:00 +1200

    implement findFirstLetter

--- __tests__/findFirstLetter.test.ts
+++ __tests__/findFirstLetter.test.ts
@@ -1,3 +1,32 @@
-describe('findFirstLetter', () => {
+import WordSearch from "../word-search"
 
+describe('findFirstLetter', () => {
+  const board = [
+    'ABCD',
+    'EFGH',
+    'IJKL',
+    'ABCD',
+  ]
+  const wordSearch = new WordSearch(board);
+
+  describe('without starting point', () => {
+    it('return undefined for letter "Z"', () => {
+      expect(wordSearch.findFirstLetter('Z')).toEqual(undefined)
+    })
+
+    it('return the point of first letter "C"', () => {
+      expect(wordSearch.findFirstLetter('C')).toEqual({x: 2, y: 0})
+    })
+  });
+
+  describe('with starting point of (2, 0)', () => {
+    const startingPoint = [2 , 0];
+    it('return undefined for letter "E"', () => {
+      expect(wordSearch.findFirstLetter('E', ...startingPoint)).toEqual(undefined)
+    })
+
+    it('return the point of second "C"', () => {
+      expect(wordSearch.findFirstLetter('C', ...startingPoint)).toEqual({x: 2, y: 3})
+    })
+  });
 })
